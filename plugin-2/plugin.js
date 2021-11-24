bacbbefbe08a0158bfa6815bd3fd7a300a410250
Alvaro Valladares <80931341+Aldhal@users.noreply.github.com>
2021-11-23 20:08:02 -0500

    Add files via upload

---
+++ plugin-2/plugin.js
@@ -0,0 +1,1 @@
+!function(){"use strict";tinymce.PluginManager.add("plugin-2",function(t,n){t.ui.registry.addButton("plugin-2",{text:"plugin-2 button",onAction:function(){t.setContent("<h2>Caracteristicas</h2>")}})})}();
